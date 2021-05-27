67239f126ab4dc8346007472dac650ec584f5bac
Guilherme Pereira <okkopsiu@gmail.com>
2021-05-26 22:45:57 -0300

    add php to send email

---
+++ php/send-email.php
@@ -0,0 +1,51 @@
+<?php
+
+// Replace this with your own email address
+$to = 'okkopsiu@gmail.com';
+
+function url()
+{
+   return sprintf(
+      "%s://%s",
+      isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https' : 'http',
+      $_SERVER['SERVER_NAME']
+   );
+}
+
+if ($_POST) {
+
+   $name = trim(stripslashes($_POST['name']));
+   $email = trim(stripslashes($_POST['email']));
+   $subject = trim(stripslashes($_POST['subject']));
+   $contact_message = trim(stripslashes($_POST['message']));
+
+
+   if ($subject == '') {
+      $subject = "Contact Form Submission";
+   }
+
+   // Set Message
+   $message .= "Email de: " . $name . "<br />";
+   $message .= "Email: " . $email . "<br />";
+   $message .= "Message: <br />";
+   $message .= nl2br($contact_message);
+   $message .= "<br /> ----- <br /> O email foi enviado de seu site " . url() . " contact form. <br />";
+
+   // Set From: header
+   $from =  $name . " <" . $email . ">";
+
+   // Email Headers
+   $headers = "From: " . $from . "\r\n";
+   $headers .= "Reply-To: " . $email . "\r\n";
+   $headers .= "MIME-Version: 1.0\r\n";
+   $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+   ini_set("sendmail_from", $to); // for windows server
+   $mail = mail($to, $subject, $message, $headers);
+
+   if ($mail) {
+      echo "OK";
+   } else {
+      echo "Algo deu errado. Por favor tente novamente.";
+   }
+}
